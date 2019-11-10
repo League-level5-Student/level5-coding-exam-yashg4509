c4499c1afc668a7b1c6813966e9af8b8afa67a27
League Student <league@iMac.attlocal.net>
2019-11-10 14:24:10 -0800

    I got 80/100!!!!!

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -7,12 +7,18 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class CodingExamA {
+	static String numRobotsInput = JOptionPane.showInputDialog("How many robots do you want?");
+	static int numRobots = Integer.parseInt(numRobotsInput);	
+		
+	static String colorInput = JOptionPane.showInputDialog("What color do you want: red, green, or blue?");
+	static Color c = Color.getColor(colorInput.toUpperCase());
+	
 	public static void main(String[] args) {
 		/*
 		 * Write a program that asks the user for three pieces of information.
 		 * 1. How many robots
 		 * 2. The color of the shapes
-		 * 3. How many sides each shape will have
+		 * 3. Using the robots, draw hexagopns
 		 * 
 		 * Once the information has been collected, the program will then make the requested number of robots
 		 * each draw the requested shape in the requested color. The robots should execute at the same time so 
@@ -21,6 +27,38 @@ public class CodingExamA {
 		 * 
 		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
 		 */
+		
+		int xPos = 100;
+		int yPos = 500;
+		Robot[] robots = new Robot[numRobots];
+		for (int i = 0; i < robots.length; i++) {
+			robots[i] = new Robot(xPos, yPos);
+			robots[i].setSpeed(10);
+			robots[i].setPenColor(c);
+			robots[i].penDown();
+			xPos += 400;
+		}
+		
+		for(Robot r: robots) {
+//			r.setSpeed(10);
+//			r.penDown();
+//			r.setPenColor(c);
+			new Thread(() -> drawPentagon(r)).start();
+		}
 
+	}
+	
+	public static void drawPentagon(Robot r) {
+//			r.setSpeed(10);
+//			r.penDown();
+//			r.setPenColor(c);
+		for (int i = 0; i < 6; i++) {
+//			r.setSpeed(10);
+//			r.penDown();
+//			r.setPenColor(c);
+			r.move(100);
+			r.turn(360/6);
+			
+		}
 	}
 }
--- src/Coding_Exam_B/CodingExamB.java
+++ src/Coding_Exam_B/CodingExamB.java
@@ -5,27 +5,63 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CodingExamB {
-	/*
+	static /*
 	 * This is a logging program for collecting TODO comments in a program. The program will scan
 	 * Through all the files in the Coding_Exam_B/classes package, and collect all the comments that start
 	 * with //TODO: and write them to their own file. See the TODO_Log_example.txt file for an idea of what 
 	 * the final file output will look like.
 	 */
+	String finale;
+	static int occurences;
+	static String fName;
+
+	static ArrayList<String> lines = new ArrayList<>();
 	
 	
 	public static String getLoggingInfo(String fileName) {
+		 fName = "File: " + fileName;
+		
 		/*
 		 * 1. Complete the getLoggingInfoMethod.
 		 *    The method takes in a String for a file and returns a String. 
 		 *    The method will open the file a read through it. It will then 
 		 *    take all the comments that begin with //TODO: and combine them 
 		 *    into one large String. The string will also state the file name and
-		 *    the line number for where each TODO was found. 
+		 *    the line number for where each TODO was found. '
 		*/
+		try {
+			
+			BufferedReader r = new BufferedReader(new FileReader(fileName));
+			
+			
+			r.lines().forEach(line -> {
+
+				if(line.contains("TODO")) {
+					lines.add(line);
+					occurences += 1;
+				}
+//				if(occurences <= 1) {
+//					finale = fName + "\n" + line;
+//					System.out.println(finale);
+//				} else {
+//					System.out.println(line);
+//				}
+
+			});
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String result = "";
 		
-		return "";
+		for (String l: lines) {
+			result = fName + "\n" + l.trim();
+		}
+		System.out.println(result );
+		return result + "\n";
 	}
 	
 	public static void main(String[] args) {
@@ -37,6 +73,17 @@ public class CodingExamB {
 		/*
 		 * 2. Write the finalLogString to a file called TODO_Log.txt. The file should match TODO_Log_example.txt. 
 		 */
-
+		
+		try {
+			FileWriter fw = new  FileWriter("src/Coding_Exam_B/TODO_Log.txt", true);
+			fw.write(finalLogString);
+			fw.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 	}
 }
